f8570d4a60f9b27b14e63ea77e24c06a28fea775
elham1986 <55925737+elham1986@users.noreply.github.com>
2019-10-04 23:18:07 +0200

    Add files via upload

---
+++ RadioMetryOfSurfaces.m
@@ -0,0 +1,16 @@
+close all
+D=50;
+r=20;
+v=[0;0;1];
+thetai= pi/2;
+phii= pi/2;
+L=[sin(thetai)*cos(phii);sin(thetai)*sin(phii);cos(thetai)];
+Z=L+v;
+Normm=norm(Z);
+H=Z./Normm;
+dd=((i-D/2)^2 +(j-D/2)^2);
+d=sqrt(dd);
+for i=:
+     j=:
+    
+end
